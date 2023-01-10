32bcbc07ee66b2721136656b9dd895e34f3a4eeb
AsifAziee <mohammed.asif@edstem.com>
2023-01-10 19:13:05 +0530

    comments resolved

--- Day2Assignment/Program.cs
+++ Day2Assignment/Program.cs
@@ -3,63 +3,63 @@
 
 int cardNumber = 12345;
 int pinNumber = 1234;
-int balance = 1000;
-int currentBalance = 0;
+int balance = 0;
+int value = 0;
+int amount = 0;
 
 
-int withdraw()
+void UserMenu()
 {
-    Console.WriteLine("Enter the amount to Withdraw :");
-    int amt = Convert.ToInt32(Console.ReadLine());
-    if (balance >= amt)
-    {
-        if (amt % 100 == 0)
-        {
-            Console.WriteLine($"Collect the amount: {amt}");
-            currentBalance = balance - amt;
-           
-        }
-        else
-        {
-            Console.WriteLine("Please enter the amount to withdraw in the multiples of 100 ");
-        }
-    }
-    else
-    {
-        Console.WriteLine("Your account does not have sufficient balance");
-    }
-    return currentBalance;
+    Console.WriteLine();
+    Console.WriteLine("Press 1 TO Check balance :");
+    Console.WriteLine("Press 2 To Withdraw the amount :");
+    Console.WriteLine("Press 3 To Deposit the amount :");
+    Console.WriteLine("Press 4 to QUIT..!");
+    Console.WriteLine();
+    Console.Write("Enter the value :");
+    value = Convert.ToInt32(Console.ReadLine());
 }
 
 
-int deposit()
+int UserDeposit(int amount)
 {
-    Console.WriteLine("Enter the amount to Deposit :");
-    int deposit = Convert.ToInt32(Console.ReadLine());
-    currentBalance = balance + deposit;
+    balance += amount;
+    Console.WriteLine("Deposite Successfully Completed");
     return balance;
 }
+
+
+void UserBalance()
+{
+    Console.WriteLine($"Current balance is: {balance}");
+}
+
+
+int UserWithdraw(int amt)
+{
+    balance -= amt;
+    Console.WriteLine("\nTransaction Successfully Completed");
+    return balance;
+}
+
 try
 {
-    Console.WriteLine("Please Enter your 5 digit card number :");
+    Console.WriteLine("Please Enter your 5 digit card number : ");
     var userNumber = Convert.ToString(Console.ReadLine());
+
     if (userNumber.Length != 5)    // Checking whether the number is 5 digit.
     {
         Console.WriteLine("You should enter 5 Digit number for your card");
         return;
     }
-    Console.WriteLine("Please enter the secret 4 digit PIN :");
+
+    Console.WriteLine("Please enter the secret 4 digit PIN : ");
     int userPin = Convert.ToInt32(Console.ReadLine());
     int userdata = Int32.Parse(userNumber); // Converting checked string to integer.
+
     if (cardNumber == userdata && pinNumber == userPin) // Checking the access.
     {
         Console.WriteLine("Access granted");
-        Console.WriteLine();
-        Console.WriteLine("What would you like to do..?");
-        Console.WriteLine("Press 1 TO Check balance :");
-        Console.WriteLine("Press 2 To Withdraw the amount :");
-        Console.WriteLine("Press 3 To Deposit the amount :");
-        Console.WriteLine("Press 4 to QUIT..!");
         Console.WriteLine();
     }
     else
@@ -67,28 +67,45 @@ try
         Console.WriteLine("Denied");  // Access denied.
         return;
     }
-        Console.Write("Enter the value :");
-        int value = Convert.ToInt32(Console.ReadLine());
+    while (!value.Equals(4))
+    {
+        UserMenu();
+        Console.Clear();
         switch (value)
         {
             case 1:
-                Console.WriteLine($"Current balance is: {balance}");
+                Console.WriteLine();
+                UserBalance();
                 break;
             case 2:
-            withdraw();
-            Console.WriteLine($"Now your current balance is {currentBalance}");
-            break;
-            case 3:                        
-                deposit();
-                Console.WriteLine($"The current balance is: {currentBalance}");
-            break;
+                Console.Write("Enter amount : ");
+                amount = Convert.ToInt32(Console.ReadLine());
+                if (amount > balance)
+                {
+                    Console.WriteLine("Your account does not have sufficient balance");
+                }
+                else
+                {
+                    UserWithdraw(amount);
+                    UserBalance();
+                    break;
+                }
+                break;
+            case 3:
+                Console.Write("Enter amount : ");
+                amount = Convert.ToInt32(Console.ReadLine());
+                UserDeposit(amount);
+                UserBalance();
+                break;
+
             case 4:
-                Console.WriteLine("You choose to exit..!");
+                Console.WriteLine("Thank you for using,..!");
                 break;
             default:
-                Console.WriteLine("Invalid");
+                Console.WriteLine("Invalid Choice ");
                 break;
         }
+    }
 
 }
 catch
